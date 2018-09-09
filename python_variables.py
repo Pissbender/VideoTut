128293307633bfab3cafc9058f1121b4b01b4082
pissbender <pissbender@nigge.rs>
2018-09-09 17:09:12 +0100

    Python primitive variables

---
+++ python_variables.py
@@ -0,0 +1,13 @@
+# character_name = "John"
+character_name = "Tom"
+# character_age = "35"
+character_age = "50"
+is_male = True
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+
+# character_name = "Mike"
+
+print("He really liked the name " + character_name + ", ")
+print("but he didn't like being " + character_age)
